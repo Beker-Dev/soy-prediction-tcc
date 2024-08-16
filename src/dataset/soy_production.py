36853a8abf9d1dc28c1f15359b4023b8bd50f2a1
Beker-Dev <joaopedro.beker@hotmail.com>
2024-08-16 20:36:17 -0300

    treat dataset empty values

--- src/dataset/soy_production.py
+++ src/dataset/soy_production.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import pandas as pd
+import numpy as np
 import json
 
 
@@ -9,8 +10,9 @@ class SoyProductionDataset:
         self.xlsx_file = xlsx_file
         self.range_years = range_years
         self.data = dict()
+        self.dataframe = None
 
-    def _set_area(self):
+    def _set_area(self) -> None:
         area_df = pd.read_excel(self.xlsx_file, sheet_name='Área plantada (Hectares)')
 
         # for some reason only this column is int
@@ -20,14 +22,18 @@ class SoyProductionDataset:
 
         self._set_attr_from_dataframe(area_df, self.data, 'area')
 
-    def _set_production(self):
+    def _set_production(self) -> None:
         production_df = pd.read_excel(self.xlsx_file, sheet_name='Quantidade produzida (Tonela...')
         self._set_attr_from_dataframe(production_df, self.data, 'production')
 
-    def _set_attr_from_dataframe(self, df: Any, obj: dict, key: str):
+    def _set_attr_from_dataframe(self, df: Any, obj: dict, key: str) -> None:
         for index, row in df.iterrows():
             city = row['Município']
             city_data = {}
+
+            if not isinstance(city, str):
+                # this avoids to add a 'NaN' city
+                continue
 
             if not obj.get(city):
                 obj[city] = {}
@@ -45,18 +51,37 @@ class SoyProductionDataset:
                 else:
                     obj[city][year] = c_data
 
-    def _handle_empty_values(self):
-        pass
-        # if isinstance(row_value, str) and not row_value.isnumeric():
-        #     # we probably should add some handler to insert a median value
-        #     row_value = 0
+    def _calculate_median(self, df_key: str) -> int:
+        values = list()
 
-    def get_dataset(self):
+        for city in self.dataframe.columns:
+            for year in self.dataframe[city].keys():
+                data_value = self.dataframe[city][year][df_key]
+                if pd.notna(data_value) and isinstance(data_value, (int, float)):
+                    values.append(data_value)
+
+        return int(np.median(values))
+
+    def _treat_empty_values(self, df_cell: dict) -> dict:
+        possible_empty_values = [None, np.nan, 'nan', '-']
+        df_keys = ['area', 'production']
+
+        if isinstance(df_cell, dict):
+            for key in df_keys:
+                df_cell[key] = (
+                    self._calculate_median(key)
+                    if df_cell[key] in possible_empty_values else df_cell[key]
+                )
+
+        return df_cell
+
+    def _handle_dataset_values(self) -> None:
+        self.dataframe = pd.DataFrame(data=self.data)
+        self.dataframe = self.dataframe.map(self._treat_empty_values)
+
+    def get_dataset(self) -> pd.DataFrame:
         self._set_area()
         self._set_production()
-        self._handle_empty_values()
-        return self.data
+        self._handle_dataset_values()
+        return self.dataframe
 
-
-dataset = SoyProductionDataset('../assets/tabela1612_mod.xlsx')
-dataset.get_dataset()
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from src.dataset.soy_production import SoyProductionDataset
+
+soy_production_dataset = SoyProductionDataset()
+print(soy_production_dataset.get_dataset())
